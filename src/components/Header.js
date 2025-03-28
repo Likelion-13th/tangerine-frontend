44c27c89e341ca69e0f5826b7d9c065d2dab4583
tangerine-01 <kgruri5@gmail.com>
2025-03-28 15:50:22 +0900

    20250328 frontend

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,13 @@ const Header = () => {
     const location = useLocation();
     const currentPage = location.pathname;
     return(
-        <div className="header-container">
-            <Link to="/" className={currentPage === "/" ? "active" : ""}> LIKELION </Link>
+        <div className={`header-container ${currentPage === "/mypage" ? "mypage-bg" : ""}`}>
+            <Link to="/" className={currentPage === "/" ? "active-home" : ""}> LIKELION </Link>
             <div className="header-subtitle-wrapper">
                 <Link to="/new" className={currentPage === "/new" ? "active" : ""}> New </Link>
                 <Link to="/perfume" className={currentPage === "/perfume" ? "active" : ""}> Perfume </Link>
                 <Link to="/diffuser" className={currentPage === "/diffuser" ? "active" : ""}> Diffuser </Link>
-                <Link to="/mypage" className={currentPage === "/mypage" ? "active" : ""}> Mypage </Link>
+                <Link to="/mypage" className={currentPage === "/mypage" ? "active-bg" : ""}> Mypage </Link>
             </div>
         </div>
     );
